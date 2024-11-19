788730ab8b462b90e9cbb247529749045c1089a8
Min Park <jmpark95@live.com>
2024-11-19 10:46:36 +0000

    transformed now_playing data

---
+++ models/transformation/now_playing_transformed.sql
@@ -0,0 +1,19 @@
+{{ config(materialized='table') }}
+
+WITH source AS 
+(
+  SELECT *
+  FROM {{ source('transformation','now_playing') }}
+)
+SELECT
+    id,
+    title,
+    genre_ids,
+    release_date,
+    original_language,
+    vote_average,
+    overview,
+    popularity,
+    vote_count
+    adult
+FROM source
